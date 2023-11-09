23283d110708cf12b6b36d45e11ef29e71ecb17e
du.pham <du.pham@lge.com>
2023-11-09 16:50:53 +0700

    Change code

--- Day2_Array_Pointer_String/main.c
+++ Day2_Array_Pointer_String/main.c
@@ -1,15 +1,55 @@
 #include <stdio.h>
- 
-int main()
-{
- 
-    //Sử dụng con trỏ;
-    char *myString = "Pham Van Du";
-    const char *myString1 = "Pham Van Du";
-	myString1 = "PhamVanDu1234567890";
-    printf("%s\n", myString);
-    printf("%s\n", myString1);
+#include <string.h>
+void bubbleSort(int arr[], int offset, int count){
+	for(int i = offset; i <= count-1; i++){
+		for(int j = i + 1; j <= count; j++){
+			if(arr[i] > arr[j]){
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
 
-    
-    return 0;
+}
+void invert(int input[], int num_element){
+	for(int i = 0; i <= num_element/2; i++){
+		int tmp = input[i];
+		input[i] = input[num_element-i-1];
+		input[num_element-i-1] = tmp;
+	}
+}
+void invert1(int input[], int num_element, int output[]){
+	memcpy(output, input, num_element*4);
+	for(int i = 0; i <= num_element/2; i++){
+		int tmp = output[i];
+		output[i] = output[num_element-i-1];
+		output[num_element-i-1] = tmp;
+	}
+}
+void swap(int **a, int **b, int **c){
+	int* tmp1 = *a;
+	int* tmp2 = *b;
+	int* tmp3 = *c;
+
+	*c = tmp1;
+	*a = tmp2;
+	*b = tmp3;
+}
+int main(){
+	int x = 10, y = 20, z = 30;
+	int *p, *q, *r;
+
+	p = &x;
+	q = &y;
+	r = &z;
+	printf("x = %d p = %#x *p = %d\n", x, p, *p);
+	printf("y = %d q = %#x *q = %d\n", y, q, *q);
+	printf("z = %d r = %#x *r = %d\n", z, r, *r);
+	printf("Swapping pointers\n");
+	swap(&p, &q, &r);
+	printf("x = %d p = %#x *p = %d\n", x, p, *p);
+	printf("y = %d q = %#x *q = %d\n", y, q, *q);
+	printf("z = %d r = %#x *r = %d\n", z, r, *r);
+	return 0;
 }
