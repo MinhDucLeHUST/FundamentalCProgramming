bef724c86453c9b1b2cf4a40ea397d97ac8846b0
MinhDucLeHUST <minhducle2082k@gmail.com>
2023-11-17 14:30:16 +0700

    update v4

---
+++ Theory/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char name[10];
+    int id;
+    float score;
+} student_t;
+
+int main() {
+    student_t studentInfo;
+    char listStudent[10];
+    return 0;
+}
